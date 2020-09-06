f22cdff1585db2eaadc3f6ccec793ece4fb053e4
Joonas Lehtoranta <joonas.lehtoranta@metropolia.fi>
2020-09-06 15:57:36 +0300

    beautifying

--- js/ajax.js
+++ js/ajax.js
@@ -9,9 +9,9 @@ $(function(){
     $.getJSON('https://api.tvmaze.com/search/shows?q=$' + haku,function(data) {
       $.each(data, function(index, value){
         if(value.show.image !== null){
-          lista.append('<li><img src="' + value.show.image.medium + '"><h3>'+ value.show.name +'</h3>'+ value.show.summary + '</li>')
+          lista.append('<li><img src="' + value.show.image.medium + '" width="210px" height="295px"><h3>'+ value.show.name +'</h3>'+ value.show.summary + '</li>')
         } else {
-          lista.append('<li><img src=""><h3>'+ value.show.name +'</h3>'+ value.show.summary + '</li>')
+          lista.append('<li><img src="https://media1.tenor.com/images/a828888852e708d9afaaad06c7f9513f/tenor.gif" width="210px" height="295px"><h3>'+ value.show.name +'</h3>'+ value.show.summary + '</li>')
         }
       });
     });
