135c4cbab0a3693c5140a5142fc372a1aae4d5c4
Joonas Lehtoranta <joonas.lehtoranta@metropolia.fi>
2020-09-22 19:00:57 +0300

    rukkausta

--- js/ajax.js
+++ js/ajax.js
@@ -2,17 +2,19 @@
 
 let lista = $('ul');
 
-$(function(){
+$(function() {
   $('#hakunappi').click(function() {
     lista.empty();
     let haku = $('#hakuteksti').val();
-    $.getJSON('https://api.tvmaze.com/search/shows?q=$' + haku,function(data) {
-      $.each(data, function(index, value){
-        if(value.show.image !== null){
-          lista.append('<li><img src="' + value.show.image.medium + '" width="210px" height="295px"><h3>'+ value.show.name +'</h3>'+ value.show.summary + '</li>')
-        } else {
-          lista.append('<li><img src="https://media1.tenor.com/images/a828888852e708d9afaaad06c7f9513f/tenor.gif" width="210px" height="295px"><h3>'+ value.show.name +'</h3>'+ value.show.summary + '</li>')
-        }
+    $.getJSON('https://api.tvmaze.com/search/shows?q=' + haku, function(data) {
+      console.log(data);
+      $.each(data, function(index, value) {
+        let tiedot = `<li><img src="${value.show.image ?
+                      value.show.image.medium :
+                      'https://media1.tenor.com/images/a828888852e708d9afaaad06c7f9513f/tenor.gif'}" width="210px" height="295px">
+                      <h3>${value.show.name}</h3> ${value.show.summary}
+                      </li>`;
+        lista.append(tiedot);
       });
     });
   });
