21c440315ad600596bc5cdefc9f751624d6bf164
Joonas Lehtoranta <joonas.lehtoranta@metropolia.fi>
2020-09-25 17:08:38 +0300

    Bootstrap ajax muokkausta WIP

--- js/ajax.js
+++ js/ajax.js
@@ -9,10 +9,17 @@ $(function() {
     $.getJSON('https://api.tvmaze.com/search/shows?q=' + haku, function(data) {
       console.log(data);
       $.each(data, function(index, value) {
-        let tiedot = `<li><img src="${value.show.image ?
-                      value.show.image.medium :
-                      'https://media1.tenor.com/images/a828888852e708d9afaaad06c7f9513f/tenor.gif'}" width="210px" height="295px">
-                      <h3>${value.show.name}</h3> ${value.show.summary}
+        let tiedot = `<li class="list-group-item">
+                        <div class="row">
+                          <div class="col-sm-2">
+                              <img src="${value.show.image ?
+                              value.show.image.medium :
+                              'https://media1.tenor.com/images/a828888852e708d9afaaad06c7f9513f/tenor.gif'}" width="210px" height="295px">
+                          </div>
+                           <div class="col-sm-10"> 
+                            <h3>${value.show.name}</h3> ${value.show.summary}
+                            </div>
+                        </div>
                       </li>`;
         lista.append(tiedot);
       });
